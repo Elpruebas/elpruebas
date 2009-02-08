b168d22fb373b0825bdf11b4ac35d39da404c095
mio <mio@mio.es>
2009-02-08 13:04:47 +0100

    otro
    Signed-off-by: mio <mio@mio.es>

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	printf("length = %d\n", strlen(name)); /* debug line */
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-	printf("Hello %s!\n", name);
+	printf("Hello, %s!\n", name);
 	return 0;
 
 	printf("mi cambio de prueba");
